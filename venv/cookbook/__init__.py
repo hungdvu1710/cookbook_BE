bc14386b4fe57f68d5435efae57b27649c861f46
Hung Vu <hungvsme@gmail.com>
2022-04-28 23:14:59 -0500

    Add user to Database

--- venv/cookbook/__init__.py
+++ venv/cookbook/__init__.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import jsonify, request, Flask, make_response
 from .extensions import mongo
-from .main import main
+from flask_cors import CORS
 
-def create_app(config_object = 'cookbook.settings'):
-  app = Flask (__name__)
+config_object = 'cookbook.settings'
+app = Flask (__name__)
+app.config.from_object(config_object)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+mongo.init_app(app)
 
-  app.config.from_object(config_object)
+@app.route('/')
+def index():
+  user_collection = mongo.db.users
+  user_collection.insert_one({'name': 'H', 'age': 17})
+  response = make_response()
+  return response
 
-  mongo.init_app(app)
-
-  app.register_blueprint(main)
-
-  return app
+@app.route('/api/user', methods = ['POST'])
+def addUser():
+  request_data = request.get_json()
+  email = request_data['email']
+  clerkGeneratedId = request_data['id']
+  user_collection = mongo.db.users
+  user_collection.find_one_and_update({'_id' : clerkGeneratedId}, {"$set": {"email": email}}, upsert=True)
+  return 'Added user to Database'
